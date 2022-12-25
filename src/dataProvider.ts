d46205aa3f011601e0659f9a70152425aa1f425e
Vitaliy Senenko <senenkovitalik@gmail.com>
2022-12-25 22:36:10 +0200

    feat: Add data provider

---
+++ src/dataProvider.ts
@@ -0,0 +1,220 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { DataProvider } from "ra-core";
+
+const omit = (keys: Partial<any>, obj: {}) =>
+  Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
+
+const apiUrl = "http://localhost:3001/graphql";
+
+const client = new ApolloClient({
+  uri: apiUrl,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
+});
+
+const fields: { [key: string]: string } = {
+  subscribers: "id firstName middleName lastName",
+};
+
+export const dataProvider: DataProvider = {
+  getList: (resource, { sort, pagination, filter }) => {
+    const { field, order } = sort;
+    const { page, perPage } = pagination;
+    return client
+      .query({
+        query: gql`
+            query ($limit: Int, $offset: Int, $order_by: [${resource}_order_by!]) {
+                ${resource}(limit: $limit, offset: $offset, order_by: $order_by) {
+                    ${fields[resource]}
+                }
+                ${resource}_aggregate {
+                    aggregate {
+                        count
+                    }
+                }
+            }`,
+        variables: {
+          limit: perPage,
+          offset: (page - 1) * perPage,
+          order_by: { [field]: order.toLowerCase() },
+          where: Object.keys(filter).reduce(
+            (prev, key) => ({
+              ...prev,
+              [key]: { _eq: filter[key] },
+            }),
+            {}
+          ),
+        },
+      })
+      .then((result) => ({
+        data: result.data[resource],
+        total: result.data[`${resource}_aggregate`].aggregate.count,
+      }));
+  },
+  getOne: (resource, params) => {
+    return client
+      .query({
+        query: gql`
+            query ($id: Int!) {
+                ${resource}_by_pk(id: $id) {
+                    ${fields[resource]}
+                }
+            }`,
+        variables: {
+          id: parseInt(params.id),
+        },
+      })
+      .then((result) => ({ data: result.data[`${resource}_by_pk`] }));
+  },
+  getMany: (resource, params) => {
+    return client
+      .query({
+        query: gql`
+            query ($where: ${resource}_bool_exp) {
+                ${resource}(where: $where) {
+                    ${fields[resource]}
+                }
+            }`,
+        variables: {
+          where: {
+            id: { _in: params.ids },
+          },
+        },
+      })
+      .then((result) => ({ data: result.data[resource] }));
+  },
+  getManyReference: (resource, { target, id, sort, pagination, filter }) => {
+    const { field, order } = sort;
+    const { page, perPage } = pagination;
+    return client
+      .query({
+        query: gql`
+            query ($limit: Int, $offset: Int, $order_by: [${resource}_order_by!], $where: ${resource}_bool_exp) {
+                ${resource}(limit: $limit, offset: $offset, order_by: $order_by, where: $where) {
+                    ${fields[resource]}
+                }
+                ${resource}_aggregate(where: $where) {
+                    aggregate {
+                        count
+                    }
+                }
+            }`,
+        variables: {
+          limit: perPage,
+          offset: (page - 1) * perPage,
+          order_by: { [field]: order.toLowerCase() },
+          where: Object.keys(filter).reduce(
+            (prev, key) => ({
+              ...prev,
+              [key]: { _eq: filter[key] },
+            }),
+            { [target]: { _eq: id } }
+          ),
+        },
+      })
+      .then((result) => ({
+        data: result.data[resource],
+        total: result.data[`${resource}_aggregate`].aggregate.count,
+      }));
+  },
+  create: (resource, params) => {
+    return client
+      .mutate({
+        mutation: gql`
+            mutation ($data: ${resource}_insert_input!) {
+                insert_${resource}_one(object: $data) {
+                    ${fields[resource]}
+                }
+            }`,
+        variables: {
+          data: omit(params.data, ["__typename"]),
+        },
+      })
+      .then((result) => ({
+        data: result.data[`insert_${resource}_one`],
+      }));
+  },
+  update: (resource, params) => {
+    return client
+      .mutate({
+        mutation: gql`
+            mutation ($id: Int!, $data: ${resource}_set_input!) {
+                update_${resource}_by_pk(pk_columns: { id: $id }, _set: $data) {
+                    ${fields[resource]}
+                }
+            }`,
+        variables: {
+          id: params.id,
+          data: omit(params.data, ["__typename"]),
+        },
+      })
+      .then((result) => ({
+        data: result.data[`update_${resource}_by_pk`],
+      }));
+  },
+  updateMany: (resource, params) => {
+    return client
+      .mutate({
+        mutation: gql`
+            mutation ($where: ${resource}_bool_exp!, $data: ${resource}_set_input!) {
+                update_${resource}(where: $where, _set: $data) {
+                    affected_rows
+                }
+            }`,
+        variables: {
+          where: {
+            id: { _in: params.ids },
+          },
+          data: omit(params.data, ["__typename"]),
+        },
+      })
+      .then((result) => ({
+        data: params.ids,
+      }));
+  },
+  delete: (resource, params) => {
+    return client
+      .mutate({
+        mutation: gql`
+            mutation ($id: Int!) {
+                delete_${resource}_by_pk(id: $id) {
+                    ${fields[resource]}
+                }
+            }`,
+        variables: {
+          id: params.id,
+        },
+      })
+      .then((result) => ({
+        data: result.data[`delete_${resource}_by_pk`],
+      }));
+  },
+  deleteMany: (resource, params) => {
+    return client
+      .mutate({
+        mutation: gql`
+            mutation ($where: ${resource}_bool_exp!) {
+                delete_${resource}(where: $where) {
+                    affected_rows
+                }
+            }`,
+        variables: {
+          where: {
+            id: { _in: params.ids },
+          },
+        },
+      })
+      .then((result) => ({
+        data: params.ids,
+      }));
+  },
+};
