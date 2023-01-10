35e51364291717e946bba45fcd9b2c191930cc86
Vitaliy Senenko <senenkovitalik@gmail.com>
2023-01-10 16:32:03 +0200

    feat: Change menu icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import {
   Resource,
 } from "react-admin";
 import GroupsIcon from "@mui/icons-material/Groups";
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import NumbersIcon from '@mui/icons-material/Numbers';
+import DynamicFormIcon from '@mui/icons-material/DynamicForm';
+
 import { dataProvider } from "./dataProvider";
 import {
   SubscriberCreate,
@@ -30,17 +34,20 @@ const App = () => (
       name="communication_types"
       list={CommunicationTypeList}
       show={CommunicationTypeShow}
+      icon={DynamicFormIcon}
     />
     <Resource
       name="communication_phone_numbers"
       list={CommunicationPhoneNumberList}
       show={ShowGuesser}
+      icon={NumbersIcon}
     />
     <Resource
       name="locations"
       list={LocationList}
       show={LocationShow}
       edit={LocationEdit}
+      icon={LocationCityIcon}
     />
   </Admin>
 );
